f4d4c33d6e243f5d24a950c46b4315a999565660
PaulPidou <pidou.paul@gmail.com>
2019-02-08 18:50:30 +0100

    Update redux

--- store/actions/actions.js
+++ store/actions/actions.js
@@ -1,8 +1,44 @@
-import { ADD_RECIPES } from './types'
+import { connect } from 'react-redux'
+import { ADD_RECIPES, REQUEST_ERROR, REQUEST_PENDING, REQUEST_SUCCESS } from './types'
 
-export const addRecipes = function(recipes) {
-    return {
-        type: ADD_RECIPES,
-        payload: recipes
+const api_ip = 'http://192.168.43.163:3000/api'
+
+export const getAllIngredients = function() {
+    return dispatch => {
+        dispatch(requestActionPending())
+        fetch(`${api_ip}/public/ingredients`)
+            .then(response => {
+                dispatch(requestActionSuccess(response.json()))
+            })
+            .catch(error => {
+                dispatch(requestActionError(error))
+            })
     }
+
+    /*
+    await new Promise(resolve => setTimeout(resolve, 1000))
+    return [
+        { "_id": "123", "name": 'abricot' },
+        { "_id": "234", "name": 'beurre' },
+        { "_id": "345", "name": 'farine' },
+        { "_id": "456", "name": 'salade' },
+        { "_id": "567", "name": 'saumon' },
+        { "_id": "678", "name": 'thon' },
+        { "_id": "789", "name": 'tortilla' },
+    ]
+    */
 }
+
+export const requestActionPending = () => ({
+    type: REQUEST_PENDING
+})
+
+export const requestActionError = (error) => ({
+    type: REQUEST_ERROR,
+    error: error
+})
+
+export const requestActionSuccess = (data) => ({
+    type: REQUEST_SUCCESS,
+    data: data
+})
