06bfa285ac78000f2bb0c34423c4aba676defee7
PaulPidou <pidou.paul@gmail.com>
2019-02-21 23:05:35 +0100

    Correct text

--- screens/FridgeScreen.js
+++ screens/FridgeScreen.js
@@ -143,7 +143,7 @@ export default class ShoppingListScreen extends React.Component {
         if (this.state.ingredients === undefined) {
             content = (<Spinner color='#007aff' />)
         } else if (this.state.ingredients.length === 0) {
-            content = (<Text>Votre frigidaire, commencez dès maintenant à la compléter !</Text>)
+            content = (<Text>Votre frigidaire est vide, commencez dès maintenant à la compléter !</Text>)
         } else {
             content = (
                 <ScrollView>
